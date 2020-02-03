49b9c0e666788ca256d1eb4dfcc0057fb4a50cba
Antoine Sauvinet <antoine.sauvinet@groupeseloger.com>
2020-02-03 16:31:17 +0100

    code structure

---
+++ Hotel.Mgmt/HotelMgmt.App/Projections/CleaningRequestReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HotelMgmt.Domain;
+using HotelMgmt.Domain.CleaningRequest;
+using HotelMgmt.Infrastructure;
+
+namespace HotelMgmt.Projections
+{
+    public class CleaningRequestReportMessageHandler : IHandleEvents
+    {
+        public void HandleEvent(IEvent @event)
+        {
+            Console.WriteLine($"I got event! : ");
+            var castedEvent = (CleaningRequested) @event;
+
+            Console.WriteLine($"[room : {castedEvent.RoomNumber}, requester : {castedEvent.RoomNumber}]");
+        }
+    }
+}
---
+++ Hotel.Mgmt/HotelMgmt.Infrastructure/Infrastructure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelMgmt.Domain;
+
+namespace HotelMgmt.Infrastructure
+{
+
+    public class RoomRepository : IRepo<Room>
+    {
+        private readonly IStoreEvents _eventStore;
+
+        public RoomRepository(IStoreEvents eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        public Room GetById(int id)
+        {
+            var eventStream = _eventStore.GetStreamForAggregate(id);
+            var room = new Room(eventStream);
+            return room;
+        }
+
+
+        public void Save(Room room)
+        {
+            _eventStore.AddEventsToStream(room.Id, room.Produced);
+            room.CommitEvents();
+
+        }
+    }
+}
